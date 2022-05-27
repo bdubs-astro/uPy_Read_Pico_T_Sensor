845d7f298f29cd9db06cd9982e6cb129e03e9a9d
bdubs-astro <bwissman@k-space.com>
2022-05-27 19:26:37 -0400

    Refactor display functions.

--- read_Pico_T_sensor.py
+++ read_Pico_T_sensor.py
@@ -5,42 +5,47 @@ connected to the fifth ADC channel.
 Typically, Vbe = 0.706 V at 27 °C, with a slope of -1.721 mV per degree.
 The ADC resolution of 16/65535 (i.e., 12 bit) equates to approx. 0.47 °C.
 
+Resources:
+
+timers:
 https://docs.micropython.org/en/latest/rp2/quickref.html#timers
 https://micropython-docs-esp32.readthedocs.io/en/esp32_doc/library/machine.Timer.html#machine-timer
 
-'''
-
-from machine import Pin, ADC, Timer, I2C
-import utime as time
-from ssd1306 import SSD1306_I2C
-import framebuf
-
-# I2C OLED setup
-from micropython import const
-oledSDA = const(4)
-oledSCL = const(5)
-WIDTH  = 128                                          
-HEIGHT = 32    
-i2c = I2C(0, scl=Pin(oledSCL), sda=Pin(oledSDA), freq=400000)
-oled = SSD1306_I2C(WIDTH, HEIGHT, i2c, 0x3C)
-
-'''
-enable larger fonts for the OLED display
+enable larger fonts for the OLED display:
 https://github.com/peterhinch/micropython-font-to-py
 https://www.youtube.com/watch?v=bLXMVTTPFMs
 https://blog.miguelgrinberg.com/post/micropython-and-the-internet-of-things-part-vi-working-with-a-screen
 '''
-import writer
-import freesans20
 
-ledBuiltin = 25
-ledDuration = 50    # ms
-led = Pin(ledBuiltin, Pin.OUT, value = 0)
+from machine import Pin, ADC, Timer
+import utime as time
+from micropython import const
 
-timerFreq = 0.5     # timer interrupt frequency (Hz)
-readSensor = Timer()
+def disp_setup(scl_pin, sda_pin, width, height, id = 0, addr = 0x3C):
+    from machine import Pin, I2C
+    from ssd1306 import SSD1306_I2C
+    
+    i2c = I2C(id, scl=Pin(scl_pin), sda=Pin(sda_pin), freq=400000)
+    oled = SSD1306_I2C(WIDTH, HEIGHT, i2c, addr)
+    return oled
 
-rtc = machine.RTC()
+def disp_string (oled, x, y, str, disp_logo = True):
+    import framebuf
+    import writer
+    import freesans20
+
+    # Raspberry Pi logo as 32x32 bytearray
+    logo = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|?\x00\x01\x86@\x80\x01\x01\x80\x80\x01\x11\x88\x80\x01\x05\xa0\x80\x00\x83\xc1\x00\x00C\xe3\x00\x00~\xfc\x00\x00L'\x00\x00\x9c\x11\x00\x00\xbf\xfd\x00\x00\xe1\x87\x00\x01\xc1\x83\x80\x02A\x82@\x02A\x82@\x02\xc1\xc2@\x02\xf6>\xc0\x01\xfc=\x80\x01\x18\x18\x80\x01\x88\x10\x80\x00\x8c!\x00\x00\x87\xf1\x00\x00\x7f\xf6\x00\x008\x1c\x00\x00\x0c \x00\x00\x03\xc0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+     
+    oled.fill(0)
+    font_writer = writer.Writer(oled, freesans20, False) # disable verbose mode
+    font_writer.set_textpos(x, y)
+    font_writer.printstring(str)
+    if disp_logo:
+        fb = framebuf.FrameBuffer(logo, 32, 32, framebuf.MONO_HLSB)  # load the framebuffer
+        # MONO_HLSB: monochrome, horizontally mapped - each byte occupies 8 horizontal pixels with bit 0 being the leftmost 
+        oled.blit(fb, 96, 0) # blit the image from the framebuffer to the display
+    oled.show()
 
 def readT():
     # internal T sensor
@@ -53,20 +58,6 @@ def readT():
     adcVolt = adcRaw * 3.3/65535
     tC = tOffs + (adcVolt - Vbe)/slope
     return tC, adcVolt, adcRaw
-
-def dispStr (x, y, str, dispLogo = True):
-    # Raspberry Pi logo as 32x32 bytearray
-    logo = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|?\x00\x01\x86@\x80\x01\x01\x80\x80\x01\x11\x88\x80\x01\x05\xa0\x80\x00\x83\xc1\x00\x00C\xe3\x00\x00~\xfc\x00\x00L'\x00\x00\x9c\x11\x00\x00\xbf\xfd\x00\x00\xe1\x87\x00\x01\xc1\x83\x80\x02A\x82@\x02A\x82@\x02\xc1\xc2@\x02\xf6>\xc0\x01\xfc=\x80\x01\x18\x18\x80\x01\x88\x10\x80\x00\x8c!\x00\x00\x87\xf1\x00\x00\x7f\xf6\x00\x008\x1c\x00\x00\x0c \x00\x00\x03\xc0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
-     
-    oled.fill(0)
-    font_writer = writer.Writer(oled, freesans20, False) # disable verbose mode
-    font_writer.set_textpos(x, y)
-    font_writer.printstring(str)
-    if dispLogo:
-        fb = framebuf.FrameBuffer(logo, 32, 32, framebuf.MONO_HLSB)  # load the framebuffer
-        # MONO_HLSB: monochrome, horizontally mapped - each byte occupies 8 horizontal pixels with bit 0 being the leftmost 
-        oled.blit(fb, 96, 0) # blit the image from the framebuffer to the display
-    oled.show() 
     
 def readISR(event):
     global ledStart
@@ -78,10 +69,27 @@ def readISR(event):
     timestring = '%02d:%02d:%02d, %02d/%02d/%04d: '%(hour, minute, sec, month, day, year)
     
     tC, adcVolt, adcRaw = readT()
-    dispStr (5, 5, ('%.1f C' %(tC)))
+    disp_string (oled, 5, 5, ('%.1f C' %(tC)))
     print(timestring + '%.2f°C, %.4fV, ADC: %d'%(tC, adcVolt, adcRaw))
    
-   
+# I2C OLED setup
+scl_pin= const(5)
+sda_pin = const(4)
+WIDTH  = 128                                          
+HEIGHT = 32
+
+oled = disp_setup(scl_pin, sda_pin, WIDTH, HEIGHT)
+
+# LED setup
+ledBuiltin = 25
+ledDuration = 50    # ms
+led = Pin(ledBuiltin, Pin.OUT, value = 0)
+
+timerFreq = 0.5     # timer interrupt frequency (Hz)
+readSensor = Timer()
+
+rtc = machine.RTC()
+
 readSensor.init(freq = timerFreq, mode = Timer.PERIODIC, callback = readISR) # frequency in Hz ... could also use period in ms
 
 while True:
