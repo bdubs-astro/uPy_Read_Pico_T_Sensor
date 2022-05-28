b7e4f9320fd102431203ccdd56ae2caf1fb148d9
Dr. Asshole <bdwissman@gmail.com>
2022-05-28 14:57:44 -0400

    Move the OLED display functions to a separate module.

--- read_Pico_T_sensor.py
+++ read_Pico_T_sensor.py
@@ -20,32 +20,7 @@ https://blog.miguelgrinberg.com/post/micropython-and-the-internet-of-things-part
 from machine import Pin, ADC, Timer
 import utime as time
 from micropython import const
-
-def disp_setup(scl_pin, sda_pin, width, height, id = 0, addr = 0x3C):
-    from machine import Pin, I2C
-    from ssd1306 import SSD1306_I2C
-    
-    i2c = I2C(id, scl=Pin(scl_pin), sda=Pin(sda_pin), freq=400000)
-    oled = SSD1306_I2C(WIDTH, HEIGHT, i2c, addr)
-    return oled
-
-def disp_string (oled, x, y, str, disp_logo = True):
-    import framebuf
-    import writer
-    import freesans20
-
-    # Raspberry Pi logo as 32x32 bytearray
-    logo = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|?\x00\x01\x86@\x80\x01\x01\x80\x80\x01\x11\x88\x80\x01\x05\xa0\x80\x00\x83\xc1\x00\x00C\xe3\x00\x00~\xfc\x00\x00L'\x00\x00\x9c\x11\x00\x00\xbf\xfd\x00\x00\xe1\x87\x00\x01\xc1\x83\x80\x02A\x82@\x02A\x82@\x02\xc1\xc2@\x02\xf6>\xc0\x01\xfc=\x80\x01\x18\x18\x80\x01\x88\x10\x80\x00\x8c!\x00\x00\x87\xf1\x00\x00\x7f\xf6\x00\x008\x1c\x00\x00\x0c \x00\x00\x03\xc0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
-     
-    oled.fill(0)
-    font_writer = writer.Writer(oled, freesans20, False) # disable verbose mode
-    font_writer.set_textpos(x, y)
-    font_writer.printstring(str)
-    if disp_logo:
-        fb = framebuf.FrameBuffer(logo, 32, 32, framebuf.MONO_HLSB)  # load the framebuffer
-        # MONO_HLSB: monochrome, horizontally mapped - each byte occupies 8 horizontal pixels with bit 0 being the leftmost 
-        oled.blit(fb, 96, 0) # blit the image from the framebuffer to the display
-    oled.show()
+import ssd1306_display_module as display
 
 def read_T():
     # internal T sensor
@@ -69,16 +44,16 @@ def read_ISR(event):
     timestring = '%02d:%02d:%02d, %02d/%02d/%04d: '%(hour, minute, sec, month, day, year)
     
     T_C, adc_volt, adc_raw = read_T()
-    disp_string (oled, 5, 5, ('%.1f C' %(T_C)))
+    display.disp_string(oled, 5, 5, ('%.1f C' %(T_C)))
     print(timestring + '%.2f°C, %.4fV, ADC: %d'%(T_C, adc_volt, adc_raw))
    
 # I2C OLED setup
-scl_pin= const(5)
-sda_pin = const(4)
+SCL_PIN = const(5)
+SDA_PIN = const(4)
 WIDTH  = 128                                          
 HEIGHT = 32
 
-oled = disp_setup(scl_pin, sda_pin, WIDTH, HEIGHT)
+oled = display.disp_setup(SCL_PIN, SDA_PIN, WIDTH, HEIGHT)
 
 # LED setup
 led_builtin = 25
@@ -94,12 +69,13 @@ read_sensor.init(freq = timer_freq, mode = Timer.PERIODIC, callback = read_ISR) 
 
 # display initial sensor reading
 T_C, adc_volt, adc_raw = read_T()
-disp_string (oled, 5, 5, ('%.1f C' %(T_C)))
+display.disp_string(oled, 5, 5, ('%.1f C' %(T_C)))
 timestamp = rtc.datetime()
 (year,month,day,xxx,hour,minute,sec,xxx) = timestamp
 timestring = '%02d:%02d:%02d, %02d/%02d/%04d: '%(hour, minute, sec, month, day, year)
 print(timestring + '%.2f°C, %.4fV, ADC: %d'%(T_C, adc_volt, adc_raw))
 
+# loop ...
 while True:
     # turn off the LED
     if (led.value() == 1 and time.ticks_diff(time.ticks_ms(), led_start) > led_duration):
---
+++ ssd1306_display_module.py
@@ -0,0 +1,26 @@
+def disp_setup(scl_pin, sda_pin, width, height, id = 0, addr = 0x3C):
+    from machine import Pin, I2C
+    from ssd1306 import SSD1306_I2C
+    
+    i2c = I2C(id, scl=Pin(scl_pin), sda=Pin(sda_pin), freq=400000)
+    oled = SSD1306_I2C(width, height, i2c, addr)
+    return oled
+
+def disp_string(oled, x, y, str, disp_logo = True):
+    import framebuf
+    import writer
+    import freesans20
+
+    # Raspberry Pi logo as 32x32 bytearray
+    logo = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|?\x00\x01\x86@\x80\x01\x01\x80\x80\x01\x11\x88\x80\x01\x05\xa0\x80\x00\x83\xc1\x00\x00C\xe3\x00\x00~\xfc\x00\x00L'\x00\x00\x9c\x11\x00\x00\xbf\xfd\x00\x00\xe1\x87\x00\x01\xc1\x83\x80\x02A\x82@\x02A\x82@\x02\xc1\xc2@\x02\xf6>\xc0\x01\xfc=\x80\x01\x18\x18\x80\x01\x88\x10\x80\x00\x8c!\x00\x00\x87\xf1\x00\x00\x7f\xf6\x00\x008\x1c\x00\x00\x0c \x00\x00\x03\xc0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+     
+    oled.fill(0)
+    font_writer = writer.Writer(oled, freesans20, False) # disable verbose mode
+    font_writer.set_textpos(x, y)
+    font_writer.printstring(str)
+    if disp_logo:
+        fb = framebuf.FrameBuffer(logo, 32, 32, framebuf.MONO_HLSB)  # load the framebuffer
+        # MONO_HLSB: monochrome, horizontally mapped - each byte occupies 8 horizontal pixels with bit 0 being the leftmost 
+        oled.blit(fb, 96, 0) # blit the image from the framebuffer to the display
+    oled.show()
+    
